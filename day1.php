ca470c570ad4a6b79747eda5b6d1bbb63bc0b83a
GabrielDJVC <djmgabriel@outlook.com>
2024-12-21 14:10:11 +0100

    commit

--- day1.php
+++ day1.php
@@ -11,18 +11,21 @@ $part2 = [];
 foreach ($arr_payload as $line) {
     $array = explode("  ", trim($line));
    
-    $part1[] = str_split($array[0]);
-    $part2[] = str_split($array[1]);
+    $part1[] = $array[0];
+    $part2[] = $array[1];
 }
 
-sort($part1[0]);
-sort($part2[0]);
-
-
-foreach ($part1 as $key => $value) {
-    $total += abs(intval($part2[0][$key]) - intval($value));
-}
+sort($part1);
+sort($part2);
 
 // echo '<pre>';
 // var_dump($total);
 // echo '</pre>';
+
+foreach ($part1 as $key => $value) {
+    $total += abs(intval($part2[$key]) - intval($value));
+}
+
+echo '<pre>';
+var_dump($total);
+echo '</pre>';
